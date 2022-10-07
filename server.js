a2702966c4999215f320eac10a8801126620bc58
mfeutseu <mfeutseu@gmail.com>
2022-10-07 09:51:08 -0400

    Completed Third-Party Express Middleware activation

--- server.js
+++ server.js
@@ -7,10 +7,26 @@ import session from "express-session";
 // ES Modules fix for _dirname
 import path, {dirname} from 'path';
 import {fileURLToPath} from 'url';
+import { lookup } from "dns";
 const _dirname = dirname(fileURLToPath(import.meta.url));
 
 // intanciate app-server
 const app = express();
+
+// setup viewEngine EJS
+app.set('views', path.join(_dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extend: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(_dirname, '../public')));
+app.use(session({
+    secret: 'MySecret',
+    saveUninitialized: false,
+    resave: false
+}))
 
 // custom middleware
 function helloPlein(req, res, next){
